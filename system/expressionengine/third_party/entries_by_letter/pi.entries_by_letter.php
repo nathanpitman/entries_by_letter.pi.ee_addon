3b2a877d2cc6afd8d7aca3c9a39b940c41c35791
Nathan Pitman <nathan@ninefour.co.uk>
2012-05-09 11:26:10 +0100

    Added an example to the docs

--- system/expressionengine/third_party/entries_by_letter/pi.entries_by_letter.php
+++ system/expressionengine/third_party/entries_by_letter/pi.entries_by_letter.php
@@ -86,7 +86,13 @@ You can use this plug-in to return entry IDs of entries matching on start letter
 
     {exp:entries_by_letter field_id="field_id_4" channel_id="2" letters="ABCDE"}
 
-You can choose to pass multiple or single letters.
+You can choose to pass multiple or single letters. An example:
+
+    {exp:channel:entries entry_id="{exp:entries_by_letter field_id='title' channel_id='1' letters='ABC'}" dynamic="no" parse="inward"}
+    {title}
+    {/exp:channel:entries}
+    
+NOTE: Make sure you include 'parse="inward"' in your channel entries tag to force the correct parsing order else this plug-in will not work.
 
     <?php
         $buffer = ob_get_contents();
